717b9bc2467cfd78b299588a3c0a2250855bb0f6
Arseniy grivvus Krupchik <kruchik.a04@gmail.com>
2025-02-26 10:08:22 +0400

    change something in nvim config

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -4,9 +4,10 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 local servers = {
-    "clangd", "pylsp", "lua_ls",
-    "cssls", "html",
-    "gopls", "jdtls", "zls",
+    clangd = {},
+    pylsp = {},
+    lus_ls = {},
+    gopls = {},
 }
 
 -- lsps with default config
