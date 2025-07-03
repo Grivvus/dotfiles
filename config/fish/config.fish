1832adeb614ddba1b75884bcc0bac384fba4483e
Arseniy grivvus Krupchik <kruchik.a04@gmail.com>
2025-07-03 22:44:43 +0400

    add clangd file config, remove conda activation from fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,20 +18,6 @@ alias vpnoff='nmcli connection down client'
 # repair wifi
 #usb_modeswitch -v 0bda -p 1a2b -J
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/grivvus/miniconda3/bin/conda
-    eval /home/grivvus/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/grivvus/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/grivvus/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/grivvus/miniconda3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
-
 # Created by `pipx` on 2024-05-26 16:17:40
 set PATH $PATH /home/grivvus/.local/bin
 
